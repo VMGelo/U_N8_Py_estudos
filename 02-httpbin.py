7448f90f4fa6aedc336ee1c84e03918defe845fa
Vinícius Mastrangelo <vinimastrangelo@gmail.com>
2026-02-04 17:12:48 -0300

    atualizacao

--- 02-httpbin.py
+++ 02-httpbin.py
@@ -5,17 +5,22 @@ url = "https://httpbin.org/post"
 data  = {
     "pessoa":{
         "nome":"Joao",
-        "idade":30
+        "profissao":"professor"
     }
+}
+params ={
+    "dataIni":"2025-01-01",
+    "dataFim":"2025-12-31"
 }
 
 # response = requests.get(url)
-response = requests.post(url, json=data)
-print(response)
+response = requests.post(url, json=data, params=params)
+print(response.request.url)
+print(response.text)
 
-with open("testes/httpbin_response.html", "w") as page:
-    page.write(response.request.url)
-    page.write("\n\n")
-    page.write(response.status_code.__str__())
-    page.write("\n\n")
-    page.write(response.text)
+#with open("testes/httpbin_response.html", "w") as page:
+#    page.write(response.request.url)
+#    page.write("\n\n")
+#    page.write(response.status_code.__str__())
+#    page.write("\n\n")
+#    page.write(response.text)
--- 03-api.py
+++ 03-api.py
@@ -3,10 +3,13 @@ from pprint import pprint
 
 nome = input("Digite o nome para pesquisa:\n")
 url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
+
 params = {
     "localidade":33 #RJ
 }
 response = requests.get(url, params=params)
+
+# tratamento de erro na requisição
 try:
     response.raise_for_status()
 except requests.HTTPError as e:
